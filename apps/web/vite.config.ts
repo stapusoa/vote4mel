0efd288938f1cca3c8210e9c35a2675d87c9f09d
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2025-06-12 19:07:47 -0600

    clean build

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/postcss'
 import autoprefixer from 'autoprefixer'
-import { cloudflare } from '@cloudflare/vite-plugin';
 
 
 export default defineConfig({
-  plugins: [react(), cloudflare()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer],
